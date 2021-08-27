c341a1e5b1d905ce7d1ffb502d9437dd965196ef
Eric Seifert <seiferteric@gmail.com>
2021-08-27 16:48:16 -0700

    Formatting

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include "cpu.h"
 #include "version.h"
 #include <stdio.h>
-int main (int argc, char* argv[]) {
-    if(argc > 1) {
-        run(argv[1]);
-    } else {
-        printf("%s Version: %d.%d\n", argv[0], vmBASIC_VERSION_MAJOR, vmBASIC_VERSION_MINOR);
-        printf("Please specify ROM file\n");
-    }
-    return 0;
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    run(argv[1]);
+  } else {
+    printf("%s Version: %d.%d\n", argv[0], vmBASIC_VERSION_MAJOR,
+           vmBASIC_VERSION_MINOR);
+    printf("Please specify ROM file\n");
+  }
+  return 0;
 }
